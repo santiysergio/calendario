02cb548738dd39e3b788af9e7a6ad5299d30dc8d
santiysergio <santiysergiocraftyo@gmail.com>
2025-07-02 12:50:13 +0200

    Primer commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+from ics import Calendar
+from flask import Flask, jsonify, send_from_directory
+from datetime import datetime, timezone
+
+app = Flask(__name__)
+
+calendars = {
+    "santiysergiocraftyo": "https://calendar.google.com/calendar/ical/santiysergiocraftyo%40gmail.com/private-c6119193fa54b29d00834ddaed15773d/basic.ics",
+    "Familia": "https://calendar.google.com/calendar/ical/family09544694029503912378%40group.calendar.google.com/private-e6ec0b523158d50e5f66fe429403578b/basic.ics",
+    "FC Barcelona": "https://calendar.google.com/calendar/ical/b31f219135d81681a6e0c5ab745ce1b72c527e5ec99f153828310f517990bade%40group.calendar.google.com/private-035eb24a9595443cde35462c9e5177ce/basic.ics",
+    "España": "https://calendar.google.com/calendar/ical/8n8ctvj3uu2ltaq1objjdtn41g%40group.calendar.google.com/public/basic.ics",
+    "Real Madrid": "https://calendar.google.com/calendar/ical/dptm8psv9fk6khmphttvmrapp4%40group.calendar.google.com/public/basic.ics",
+    "Sevilla": "https://calendar.google.com/calendar/ical/4g31en3kv4eqeuvbl8hrtd87v0%40group.calendar.google.com/public/basic.ics",
+}
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/events')
+def get_events():
+    now = datetime.now(timezone.utc)
+    all_events = []
+    for name, url in calendars.items():
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+            c = Calendar(r.text)
+            future_events = [e for e in c.events if e.begin >= now]
+            events_sorted = sorted(future_events, key=lambda e: e.begin)[:5]
+            for e in events_sorted:
+                all_events.append({
+                    "calendar": name,
+                    "summary": e.name,
+                    "start": e.begin.isoformat(),
+                    "end": e.end.isoformat(),
+                })
+        except Exception as ex:
+            all_events.append({
+                "calendar": name,
+                "error": f"No se pudo cargar el calendario: {ex}"
+            })
+    all_events = sorted(all_events, key=lambda x: x.get("start", ""))
+    return jsonify(all_events)
+
+if __name__ == '__main__':
+    app.run(debug=True)
